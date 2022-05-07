3c476e1668ea65815477429ed8d0addcebde4c4a
Diego <styx5242@gmail.com>
2022-05-07 14:14:04 -0700

    Added manual refresh if refresh token fails

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from google.auth.exceptions import RefreshError
 
 import json
 import webbrowser
@@ -339,7 +340,13 @@ def authenticate():
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            try:
+                creds.refresh(Request())
+            except RefreshError:
+                # Removes the existing file containing the user's credentials
+                os.system("rm token.json")
+                # Retries authentication
+                return authenticate()
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
